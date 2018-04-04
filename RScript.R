c1b798dfb8b33b8dbdce974c3b217226328b1692
Kesouu <38074171+Kesouu@users.noreply.github.com>
2018-04-04 15:35:36 +0200

    Update RScript.R

--- RScript.R
+++ RScript.R
@@ -1,19 +1,14 @@
 ## >>>
-### 2. Remove columns with fixed patterns in Prophane summary
-### a. Function - Remove columns with "unique_spectra_of_"
+### 2. Remove columns or patterns in Prophane summary
+### a. Function - Remove columns with "unique_spectra_of_" and remove "tr|" in the "hit.list" column
 ## >>>
 
 supp_colonnes_df <- function(my_data, ...){
-  part1 <- select(my_data, -contains("unique.spectra.of."))
-  part2 <- part1[, !names(part1) %in% c("considered.hits", "excluded.hits", 
-                                        "maximal.sequence.length", "lowest.percent.identity")]
-##  >>>
-### b. Function - Remove "tr|" in the "hit.list" column
-##  >>>
-  tmp.hitlist = gsub("tr|", '', part2[,"hit.list"], fixed=TRUE)
+  part1 <- select(my_data, -contains("unique.spectra.of."),-one_of(c("considered.hits", "excluded.hits","maximal.sequence.length", "lowest.percent.identity"))) 
+  tmp.hitlist = gsub("tr|", '', part1[,"hit.list"], fixed=TRUE)
   head(tmp.hitlist)
-  part2[,"hit.list"] = tmp.hitlist
-  return(part2)
+  part1[,"hit.list"] = tmp.hitlist
+  return(part1)
 }
 
 ## >>>
