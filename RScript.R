923993c20805678bdb5ceeac5276812f1a2de754
Kesouu <38074171+Kesouu@users.noreply.github.com>
2018-04-04 15:06:36 +0200

    Update and rename RScript to RScript.R

--- RScript.R
+++ RScript.R
@@ -1,6 +1,6 @@
 ## >>>
-### 2. Suppression de colonnes inutiles contenant des patterns précis
-### a. Fonction - Suppression des colonnes "unique_spectra_of_" et des colonnes dont les noms sont fixés par Prophane
+### 2. Remove columns with fixed patterns in Prophane summary
+### a. Function - Remove columns with "unique_spectra_of_"
 ## >>>
 
 supp_colonnes_df <- function(my_data, ...){
@@ -8,7 +8,7 @@ supp_colonnes_df <- function(my_data, ...){
   part2 <- part1[, !names(part1) %in% c("considered.hits", "excluded.hits", 
                                         "maximal.sequence.length", "lowest.percent.identity")]
 ##  >>>
-### b. Fonction - Suppression des "tr|" dans la colonne "hit.list"
+### b. Function - Remove "tr|" in the "hit.list" column
 ##  >>>
   tmp.hitlist = gsub("tr|", '', part2[,"hit.list"], fixed=TRUE)
   head(tmp.hitlist)
@@ -17,7 +17,7 @@ supp_colonnes_df <- function(my_data, ...){
 }
 
 ## >>>
-### d.  Merge en fonction de la colonne hit.list
+### d.  Merge Prophane summary and CAZyme file by hit.list column
 ## >>>
 
 merge_func <- function(my_data1, my_data2, ...){
